d5af3828676eb16a9e6b1804a83f68a84513cb51
Amogh Parmeswaran <amogh1118@gmail.com>
2020-05-08 19:50:12 -0500

    Committing final program updated with both functions to GitHub, created two successful functions both makeCacheMatrix and cacheSolve that take the inverses of matrices and caches it so that if the matrix has already been cached it simply pulls that stored data and returns cached data.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may 
+## be some benefit to caching the inverse of a matrix rather than 
+## compute it repeatedly (there are also alternatives to matrix 
+## inversion that we will not discuss here). Your assignment is to 
+## write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache 
+## its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+## Manipulating the makeVector function we were to instead of 
+## taking a vector and return its mean, taking a matrix
+## and returning its inverse
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), then the 
+## cachesolve should retrieve the inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+## Return a matrix that is the inverse of 'x'
+## Manipulating the cachemean function and instead of caching
+## means of vectors manipulating it to cache inverses of 
+## vectors.
+        
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- inverse(data, ...)
+        x$setinverse(i)
+        i
 }
